5e80f8d0c5c038c0ba73effd6d4ff3d80e141b48
AlessandroMal <ale.gooogle@gmail.com>
2020-12-01 22:42:07 +0000

    aggiunte le mie modifiche in update

---
+++ update/main.py
@@ -0,0 +1,48 @@
+import mapsfunction as mf
+import scipy.ndimage.morphology as mph
+import numpy as np
+
+Npx=300
+pxlen=1 #nm
+
+#MAP-------------------------------------------------
+
+z=mf.genFlat(Npx)
+#z=mf.genNormNoise(z,pxlen,2,50)
+#z=mf.genSphere(z,pxlen,np.array([100,100,20,70]),np.array([10,30]))
+#z=mf.genUnifSph(z,pxlen,10,5,10) #, xmin=22, xmax=58, ymin=62, ymax=78)
+#z=mf.genNormSph(z,pxlen,8,15,5) #, xmin=22, xmax=58, ymin=62, ymax=78)
+
+print(mf.specArea(z,pxlen))
+
+z=mf.genUnifIsolSph(z,pxlen,8,20,20) #, xmin=22, xmax=58, ymin=62, ymax=78)
+#OCCHIO AI DATI IN INPUT PERCHE FUNZIONA A RIGETTO
+
+print(mf.specArea(z,pxlen))
+
+#TIP------------------------------------------------
+
+tip=mf.genParabolicTip(pxlen,80,4) #occhio che h/a>>pxlen
+#tip=mf.genPyramidTip(pxlen,50,2) #occhio che h/a>>pxlen
+#----------la dilation non funziona bene con piramide ??
+#tip=mf.genSemisphTip(pxlen,40,1)
+
+#IMG------------------------------------------------
+
+img = mph.grey_dilation(z, structure=-tip)
+#---------------------------------------------------
+
+print(8*2/3*3.14*20**3+8*3.14*20**3,"  ",mf.V(z,pxlen),"  ",mf.V(img,pxlen))
+
+#PLOT-----------------------------------------------
+mf.plotview(z,pxlen,30,30)
+#mf.plotview(z,pxlen,90,0)
+#mf.plotview(z,pxlen,0,90)
+mf.plotfalsecol(z,pxlen)
+#mf.plotview(img,pxlen,90,0)
+mf.plotview(img,pxlen,30,30)
+mf.plotview(img,pxlen,0,90)
+mf.plotfalsecol(img,pxlen)
+#mf.plotview(tip,pxlen,30,30)
+#mf.plotview(tip,pxlen,90,0)
+#mf.plotview(tip,pxlen,0,90)
---
+++ update/mapsfunction.py
@@ -0,0 +1,229 @@
+import numpy as np
+from random import uniform
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+def genFlat(Npx):
+    z = np.zeros([Npx, Npx])
+    return z
+
+
+def genNormNoise(z,pxlen,var,Ltile):
+    l=int(Ltile/pxlen)
+    ntiles=int(len(z)/l)
+    
+    if len(z)%l!=0:
+        ntiles+=1
+    
+    for i in range(ntiles):
+        for j in range(ntiles):
+            r=np.random.normal(0, var)
+            for x in range(l):
+                for y in range(l):
+                    if j*l+y<len(z) and i*l+x<len(z):
+                        z[j*l+y,i*l+x]+=r
+    return z
+
+'''
+    if len(z)%l!=0:
+        for i in range(ntiles):
+            r=np.random.normal(0, var)
+            for x in range(l):
+                for y in range(l-len(z)%l):
+                    z[ntiles*l+y,i*l+x]+=r
+        
+        for i in range(ntiles):
+            r=np.random.normal(0, var)
+            for y in range(l):
+                for x in range(l-len(z)%l):
+                    z[i*l+y,ntiles*l+x]+=r
+                    
+        for x in range(l-len(z)%l):
+            for y in range(l-len(z)%l):
+ '''                          
+    
+def plotview(z,pxlen,theta,phi):
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+
+    X = np.linspace(0,pxlen*len(z),num=len(z))
+    Y = np.linspace(0,pxlen*len(z),num=len(z))
+    X, Y = np.meshgrid(X, Y)
+
+    ax.plot_surface(X,Y,z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+    ax.view_init(theta,phi)
+    
+    #ax.set_xlim(0, 10)
+    #ax.set_ylim(0, 10)
+    #ax.set_zlim(0, 10)
+    ax.set_xlabel('X (nm)')
+    ax.set_ylabel('Y (nm)')
+    ax.set_zlabel('Z (nm)')
+    
+def plotfalsecol(z,pxlen):
+    plt.figure()
+    #plt.title()
+    #plt.axis('equal')
+    
+    X = np.linspace(0,pxlen*len(z),num=len(z))
+    Y = np.linspace(0,pxlen*len(z),num=len(z))
+    X, Y = np.meshgrid(X, Y)
+    
+    plt.pcolormesh(z)
+    plt.colorbar()    
+
+def genSphere(z,pxlen,centre,R):
+    if(len(centre)!=len(R)*2):
+        print("input genSphere non validi")
+        return 0 #controllo input
+    
+    for i in range(len(R)):
+        
+        x0 = centre[2*i]
+        y0 = centre[2*i+1]
+        
+        for x in range(len(z)):
+            for y in range(len(z)):
+                if R[i]**2 - (x*pxlen - x0)**2 - (y*pxlen - y0)**2 > 0:
+                    z[y,x] += np.sqrt(R[i]**2 - (x*pxlen - x0)**2 - (y*pxlen - y0)**2) + R[i]
+    return z
+
+def genUnifSph(z,pxlen,Nsph,rmin,rmax, **kwargs):
+    xmin=kwargs.get('xmin',0)
+    ymin=kwargs.get('ymin',0)
+    xmax=kwargs.get('xmax',pxlen*len(z))
+    ymax=kwargs.get('ymax',pxlen*len(z))
+        
+    for i in range(Nsph):
+        R  = uniform(rmin, rmax)
+        x0 = uniform(xmin, xmax)
+        y0 = uniform(ymin, ymax)
+        
+        for x in range(len(z)):
+            for y in range(len(z)):
+                if R**2 - (x*pxlen - x0)**2 - (y*pxlen - y0)**2 > 0:
+                    z[y,x]+=np.sqrt(R**2 - (x*pxlen - x0)**2 - (y*pxlen - y0)**2) + R
+
+    return z
+
+def genUnifIsolSph(z,pxlen,Nsph,rmin,rmax, **kwargs):
+    xmin=kwargs.get('xmin',0)
+    ymin=kwargs.get('ymin',0)
+    xmax=kwargs.get('xmax',pxlen*len(z))
+    ymax=kwargs.get('ymax',pxlen*len(z))
+    
+    n=0
+    xyr=[]
+
+    while n<Nsph:
+        R  = uniform(rmin, rmax)
+        x0 = uniform(xmin, xmax)
+        y0 = uniform(ymin, ymax)
+        
+        b=True
+        for i in range(int(len(xyr)/3)):
+           if  R+xyr[3*i+2]>np.sqrt((x0-xyr[3*i])**2+(y0-xyr[3*i+1])**2):
+               b=False
+        
+        if b==True:
+            n+=1
+            xyr.append(x0)
+            xyr.append(y0)
+            xyr.append(R)
+            
+            for x in range(len(z)):
+                for y in range(len(z)):
+                    if R**2 - (x*pxlen - x0)**2 - (y*pxlen - y0)**2 > 0:
+                        z[y,x]+=np.sqrt(R**2 - (x*pxlen - x0)**2 - (y*pxlen - y0)**2) + R
+
+    return z
+
+def genNormSph(z,pxlen,Nsph,av,var, **kwargs):
+    xmin=kwargs.get('xmin',0)
+    ymin=kwargs.get('ymin',0)
+    xmax=kwargs.get('xmax',pxlen*len(z))
+    ymax=kwargs.get('ymax',pxlen*len(z))
+
+    for i in range(Nsph):
+#uso una tecnica di rigetto per evitare R negativi se metto
+#media e varianza pericolose
+#Ale
+        while 1>0:
+            R = np.random.normal(av, var)
+            if R>0:
+                break
+        
+        x0 = uniform(xmin,xmax)
+        y0 = uniform(ymin,ymax)
+        
+        for x in range(len(z)):
+            for y in range(len(z)):
+                if R**2 - (x*pxlen - x0)**2 - (y*pxlen - y0)**2 > 0:
+                    z[y,x]+=np.sqrt(R**2 - (x*pxlen - x0)**2 - (y*pxlen - y0)**2) + R
+
+    return z
+
+def genParabolicTip(pxlen,h,aspectratio):
+    #OCCHIO: PER AVERE UNA BUONA FIGURA MI SERVE l>>pxlen
+    l=h/aspectratio
+    px=int(l/pxlen)
+    z=np.zeros([px,px])
+    
+    for x in range(len(z)):
+        for y in range(len(z)):
+            if (l/2)**2 - (x*pxlen - px/2*pxlen)**2 - (y*pxlen - px/2*pxlen)**2 > 0:
+                z[y,x]=4*aspectratio/l*((x*pxlen - px/2*pxlen)**2 + (y*pxlen - px/2*pxlen)**2)
+            else:
+                z[y,x]=h
+    return z
+
+def genPyramidTip(pxlen,h,aspectratio):
+    #OCCHIO: PER AVERE UNA BUONA FIGURA MI SERVE l>>pxlen
+    l=h/aspectratio
+    px=int(l/pxlen)
+    z=np.zeros([px,px])
+    
+    for x in range(len(z)):
+        for y in range(len(z)):
+                z[y,x]=2*aspectratio*max(abs(x*pxlen-px/2*pxlen), abs(y*pxlen-px/2*pxlen))
+            
+    return z
+
+def genSemisphTip(pxlen,h,aspectratio):
+    #OCCHIO: PER AVERE UNA BUONA FIGURA MI SERVE l>>pxlen
+    l=h/aspectratio
+    px=int(l/pxlen)
+    z=np.ones([px,px])*h
+    R=l/2
+    
+    for x in range(len(z)):
+        for y in range(len(z)):
+            r2= (x*pxlen - px/2*pxlen)**2 + (y*pxlen - px/2*pxlen)**2
+            if r2 < R**2:
+                z[y,x]=R*(1-np.sqrt(1-r2/R**2))
+
+    return z
+
+def V(z,pxlen):
+    somma=0
+    
+    for x in range(len(z)):
+        for y in range(len(z)):
+            somma+=z[y,x]
+    
+    return pxlen*pxlen*somma
+
+def specArea(z,pxlen):
+    A=0
+    for x in range(len(z)-1):
+        for y in range(len(z)-1):
+            if x==0: a=np.array([pxlen,0,z[y,x+1]-z[y,x]])
+            else: a=a*(-1)
+            b=np.array([0,pxlen,z[y+1,x]-z[y,x]])
+            A+=np.linalg.norm(np.cross(a,b))/2
+            
+            a=np.array([-pxlen,0,z[y+1,x]-z[y+1,x+1]])
+            b=np.array([0,-pxlen,z[y,x+1]-z[y+1,x+1]])
+            A+=np.linalg.norm(np.cross(a,b))/2
+            
+    return A/(pxlen*pxlen*len(z)*len(z))
