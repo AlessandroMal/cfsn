da3b54e623d376009ce443a66994f8975822eb84
Lukas Hermans <lukas.hermans@studenti.unimi.it>
2021-01-09 22:05:16 +0100

    improved parameters estimation

--- part_num/part_num.py
+++ part_num/part_num.py
@@ -4,17 +4,31 @@ import parameters as par
 import scipy.ndimage.morphology as mph
 import matplotlib.pyplot as plt
 
-def reconstLogNorm(z, pxlen, thres, N_part):
+def reconstLogNorm(z, pxlen, thres, N_part, R_mu_real, R_sigma_real):
     z_obj_list, z_labeled = mf.identObj(z, thres)
     mf.plotThres(z, z_labeled, pxlen, 'found ' + str(len(z_obj_list)) + ' of ' + str(N_part) + ' particles, thres=' + str(thres)+'nm')
     
-    R_list = [(np.max(np.shape(obj_i))-1)*pxlen/2 for obj_i in z_obj_list]
+    R_list = [(np.max(np.shape(obj_i)))*pxlen/2 for obj_i in z_obj_list]
     
     R_mean = np.mean(R_list)
-    R_std = np.std(R_list, ddof=1)
+    R_std = np.std(R_list)
     
     R_mu = np.log(R_mean / np.sqrt(1 + R_std**2 / R_mean**2)) # recalculated gaussian
     R_sigma = np.sqrt(np.log(1 + (R_std/R_mean)**2)) # reculaculated gaussian
+    
+    x = np.linspace(R_mean - 3*R_std, R_mean + 3*R_std, 1000)
+    pdf = 1 / (x * R_sigma * np.sqrt(2*np.pi)) * np.exp(-(np.log(x) - R_mu)**2 / (2*R_sigma**2))
+    pdf_real = 1 / (x * R_sigma_real * np.sqrt(2*np.pi)) * np.exp(-(np.log(x) - R_mu_real)**2 / (2*R_sigma_real**2))
+    
+    plt.figure()
+    plt.hist(R_list, bins=12, density=True, edgecolor='black', linewidth=2, color='grey', alpha=0.5)
+    plt.plot(x, pdf, color='r', linewidth=3.5, label='empiric distribution (R_mu = {0:.3f}, R_sigma = {1:.3f})'.format(R_mu, R_sigma))
+    plt.plot(x, pdf_real, color='green', linewidth=3.5, label='real distribution (R_mu = {0:.3f}, R_sigma = {1:.3f})'.format(R_mu_real, R_sigma_real))
+    plt.xlabel(r'$R_{part} [nm]$')
+    plt.ylabel('frequency')
+    plt.title('found ' + str(len(z_obj_list)) + ' of ' + str(N_part) + ' particles, thres=' + str(thres)+'nm')
+    plt.legend(loc=1)
+    plt.tight_layout()
 
     return R_mean, R_std, R_mu, R_sigma
     
